a20aadb69c1cbde4813ea2338d5fa87f66d84137
Sandra <sandra.boez@gmail.com>
2021-03-25 12:36:57 +0100

    :rocket: Add first version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Webpack-Boilerplate
-My simple Webpack boilerplate to start all my projects
+
+<img src="https://img.shields.io/badge/Webpack-5.22-orange">
+
+# Usage
+
+```shell
+git clone https://github.com/sboez/Webpack-Boilerplate.git
+```
+
+```shell
+npm install
+```
+
+```shell
+npm run dev
+```
---
+++ config/webpack.common.js
@@ -0,0 +1,77 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+	entry: path.resolve(__dirname, '../src/App.js'),
+	output:
+	{
+		filename: 'bundle.[contenthash].js',
+		path: path.resolve(__dirname, '../dist')
+	},
+	devtool: 'source-map',
+	plugins:
+		[
+			new CopyWebpackPlugin({
+				patterns: [
+					{ from: path.resolve(__dirname, '../src/assets') }
+				]
+			}),
+			new HtmlWebpackPlugin({
+				template: path.resolve(__dirname, '../src/index.html'),
+				minify: true
+			})
+		],
+	module:
+	{
+		rules:
+			[
+				{
+					test: /\.(html)$/,
+					use: ['html-loader']
+				},
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use:
+						[
+							'babel-loader'
+						]
+				},
+				{
+					test: /\.s[ac]ss$/i,
+					use: [
+						"style-loader",
+						"css-loader",
+						"sass-loader"
+					]
+				},
+				{
+					test: /\.(jpg|png|gif|svg)$/,
+					use:
+						[
+							{
+								loader: 'file-loader',
+								options:
+								{
+									outputPath: 'assets/images/'
+								}
+							}
+						]
+				},
+				{
+					test: /\.(ttf|eot|woff|woff2)$/,
+					use:
+						[
+							{
+								loader: 'file-loader',
+								options:
+								{
+									outputPath: 'assets/fonts/'
+								}
+							}
+						]
+				},
+			]
+	}
+}
